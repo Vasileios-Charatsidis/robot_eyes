e670998e272a6e7bf5ad1de23dbd27e18919790e
Auke Wiggers <wiggers.auke@gmail.com>
2014-04-16 18:59:04 +0200

    Final changes to matlab code include basic svd

--- 1_icp/icp.m
+++ 1_icp/icp.m
@@ -1,34 +1,55 @@
 function icp(source, target)
    % Execute ICP for a given set of source and target points, which are 
    % of the same dimension.
-   %
 
-   % initialize R as identity
-   R = eye(3);
+   N = size(source, 2);
+   
+   % initialize R as identity, t as zero
+   R = eye(4);
+   t = [0; 0; 0; 0];
+   
+   % Add number of random trees to the params
+   params = struct('algorithm', 'kdtree', 'trees', 10, 'checks', 128);
+   % TODO Build index for repeated use?
    
    RMS_changed = 1;
-   while RMS_changed == 1 do 
+   RMS = rms(source, target(:, 1:N));
+   
+   while RMS_changed == 1
+      
+      fprintf('RMS: %d\n', RMS);
+      pause
+      
+      % transform source according to R and t
+      transformed_source = R * source + repmat(t, 1, N);
+      centroid_source = mean(source,2)
+      
       % find closest points using kdtree
-       
+      [results, dists] = ...
+         flann_search(transformed_source, target, 1, params);
+      disp(transformed_source)
+      disp(results)
+      
+      new_RMS = sqrt(sum(dists(:))/N);
+      if new_RMS == RMS
+          RMS_changed = 0;
+          break;
+      end
+      RMS = new_RMS;
       
       % redefine R and t using SVD
-      N = size(source, 1);
-      centroid_source = mean(source);
-      centroid_target = mean(target); 
+      selected_target = target(:, results);
+      disp(selected_target)
       
-      cova = (source - repmat(centroid_source, N, 1))' * ...
-             (target - repmat(centroid_target, N, 1));
+      centroid_selected_target = mean(selected_target, 2)
+      
+      cova = (source - repmat(centroid_source, 1, N)) * ...
+             (target - repmat(centroid_selected_target, 1, N))';
+
       [U,~,V] = svd(cova);
       % Find rotation matrix
-      R = V * U';
+      R = V * U'
       % Find translation
-      t = -R * centroid_source' + centroid_target';
-
-      % recompute RMS
-      RMS_new = rms(source, target, mapping); 
-      if RMS_new == RMS
-          RMS = RMS_new;
-          RMS_changed = 0;
-      end
+      t = -R * centroid_source + centroid_selected_target
    end
 end
--- 1_icp/main.m
+++ 1_icp/main.m
@@ -1,12 +1,26 @@
 function main(dirname)
    % Read files etc.
    
-   for i=0:2
-     filename_source = createfilename(i); 
-     filename_target = createfilename(i+1);
-     % Read pointcloud files
-     source = readPcd(sprintf('%s/%s', dirname, filename_source));
-     target = readPcd(sprintf('%s/%s', dirname, filename_target));
-     
-   end
+   %for i=0:2
+   %  filename_source = createfilename(i); 
+   %  filename_target = createfilename(i+1);
+   %  % Read pointcloud files
+   %  source = readPcd(sprintf('%s/%s', dirname, filename_source));
+   %  target = readPcd(sprintf('%s/%s', dirname, filename_target));
+   % 
+   %end
+   
+   % Each column is a point!
+   
+   pointcloud1 = [1 0 0 ;
+                  0 1 0 ;
+                  0 0 1 ;
+                  1 1 1 ];
+   pointcloud2 = [0 -1 0 ;
+                  1 0 0 ;
+                  0 0 1 ;
+                  1 1 1 ];
+   %pointcloud1 = vertcat(randn(3, 5), zeros(1, 5));
+   %pointcloud2 = vertcat(randn(3, 5), zeros(1, 5));
+   icp(pointcloud1, pointcloud2);
 end

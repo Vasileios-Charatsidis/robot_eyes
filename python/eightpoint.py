42de3ff73179baf9b57f95e82ee04f01f3ddb764
Chiel Kooijman <chiel999@gmail.com>
2014-04-30 16:45:43 +0200

    Untested first version of fundamental().

--- python/eightpoint.py
+++ python/eightpoint.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import cv
 
 
@@ -9,3 +9,36 @@ import cv
 # 2. Characterize local appearance of the regions around interest points
 # 3. Get a set of supposed matches
 # 4. Estimate fundamental matrix
+
+def fundamental(matches):
+    #A = np.array([[match[0][0] * match[1][0],
+    #               match[0][0] * match[1][1],
+    #               match[0][0],
+    #               match[0][1] * match[1][0],
+    #               match[0][1],
+    #               match[1][0],
+    #               match[1][1],
+    #               1] for match in matches])
+
+    A = np.tile(matches[0], (1, 3)) * np.repeat(matches[1], 3, 1)
+
+    U, D, V = np.linalg.svd(A)
+
+    Uf = U[:,0:3]
+    Df = np.diag(D[0:3])
+    Vf = V[0:3]
+
+    # TODO make singular. It says we have to do this in the assingment, but can
+    # not find the reference mentioned.
+
+    F = Uf * Df * Vf
+
+    Uf, Df, Vf = np.linalg.svd(F)
+
+    # Set smallest singular value to zero
+    Df[3, 3] = 0
+
+    # Recompute F
+    F = Uf * Df * Vf
+
+    return F

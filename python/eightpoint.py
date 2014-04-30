235ad59949624ded707d8d53118c92f123b6a6ca
Auke Wiggers <wiggers.auke@gmail.com>
2014-04-30 15:18:19 +0200

    eightpoint stub

---
+++ python/eightpoint.py
@@ -0,0 +1,9 @@
+import numpy
+import cv
+
+
+
+# 1. Detect interest points in both images
+# 2. Characterize local appearance of the regions around interest points
+# 3. Get a set of supposed matches
+# 4. Estimate fundamental matrix

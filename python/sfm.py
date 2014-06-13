7e4655f4d9caacf74b4321c1e99271e4c47b2ab6
Auke Wiggers <wiggers.auke@gmail.com>
2014-06-13 12:09:00 +0200

    Removed unnecessary delaunay plot

--- python/sfm.py
+++ python/sfm.py
@@ -56,18 +56,11 @@ def structure_from_motion(pointviewmat, args):
         utils.writepcd(args.output_file, S)
     if not args.no_visualization:
         fig = plt.figure(figsize=plt.figaspect(.5))
-        ax1 = fig.add_subplot(121, projection='3d')
-        ax1.scatter(S[:, 0], S[:, 1], S[:, 2])
-        ax1.set_xlabel('x')
-        ax1.set_ylabel('y')
-        ax1.set_zlabel('z')
-
-        ax2 = fig.add_subplot(122, projection='3d')
-        ax2.plot_trisurf(S[:, 0], S[:, 1], S[:, 2])
-        ax2.set_xlabel('x')
-        ax2.set_ylabel('y')
-        ax2.set_zlabel('z')
-
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(S[:, 0], S[:, 1], S[:, 2])
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
         plt.show()
 
 
